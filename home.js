e1588a267996431237047e539eaf327bcb585992
Brian Toney <btoney1988@gmail.com>
2020-09-08 13:34:13 -0400

    small adjustments to syntax

--- home.js
+++ home.js
@@ -2,16 +2,18 @@
 $("#start-button").click(function () {
   window.location.href = "quiz.html";
 });
+
 // High score function to display high scores when clicked
 $("#highscore-button").click(function () {
+
   // JSON parse in order to get the string from the local storage
   var highScores = JSON.parse(localStorage.getItem("highScores"));
   // When high score button is clicked it will add new element "li" with the top 5 scores inserted
-    highScores.map(function(score) {
-    var highScoreFunc = score.name + "-- " + score.score;   
+  highScores.map(function (score) {
+    var highScoreFunc = score.name + "-- " + score.score;
     $("#high-score").append("<li>" + highScoreFunc + "</li>");
   });
- 
+  
   // Disable the high score button in order to prevent user from clicking it multiple times
   this.disabled = true;
 });
